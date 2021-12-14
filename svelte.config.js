c36c1714d213489be579ffbecbcea13e9fb3b770
Andriy Semenistiy <46621668+MeowningMaster@users.noreply.github.com>
2021-12-15 01:52:31 +0200

    config project

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,16 @@
 import vercel from '@sveltejs/adapter-vercel';
+import { resolve } from 'path';
 
 export default {
   kit: {
     adapter: vercel(),
     target: '#svelte',
-  },
+    vite: {
+      resolve: {
+        alias: {
+          '@components': resolve('./src/components')
+        }
+      }
+    }
+  }
 };
