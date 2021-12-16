97f83dc338674f52ee2c29814f0cc28a0b900774
chabanyukm <chabanyuk.maks@gmail.com>
2021-12-16 20:30:28 +0200

    Co-authored-by: Andrii Semenystyi <MeowningMaster@users.noreply.github.com>

--- src/lib/calc.ts
+++ src/lib/calc.ts
@@ -1,6 +1,7 @@
 import { bellmanStep } from "./step";
 import { BellmanExpression } from "./step";
 import { InFunctional } from "./step";
+import { max, min } from "./util";
 
 export type Functional = {
   xInner: number[]; // _x1 & _x2
@@ -15,6 +16,11 @@ export type Constraints = number[][]; // [[...], ... , [...]]  n + 1 inner array
 export type Result = {
   x: number[][]; // [[x10, ..., x1n], [x20, ..., x2n]]
   u: number[]; // [u0, ..., u(n - 1)]
+
+  bellmanSteps?: {
+    x: number[],    // coeffs before x_k
+    freeNum: number // obtained from chosen u(i)
+  }[]      // (n + 1) times calc Bellman function
 };
 
 export const calc = (
@@ -25,16 +31,12 @@ export const calc = (
   uConstraints: Constraints // обмеження на u(i)
 ): Result => {
 
-  const countX = xInnitial.length
+  /**
+   * number of phase coordinates
+   */
+  const countXk = xInnitial.length
 
-  const xResArray: number[][] = [];
-
-  for (let i = 0; i < countX; i ++) {
-    const row = [];
-    for (let j = 0; j < n + 1; ++j) {
-      row.push(0);
-    }
-  }
+  const xResArray: number[][] = new Array(countXk).fill(0).map(() => new Array(0));
   
   const uResArray: number[] = [];
 
@@ -65,24 +67,70 @@ export const calc = (
     uResArray.unshift(bellmanExpr.chosenU)
   }
 
-  // test logging
-  console.log(uResArray);
+  let xVals: Array<number> = new Array(countXk).fill(0)
 
-  /*
-  const xResArray: number[][] = [];
+  // choose x_k(0) TESTED
+  for (let k = 0; k < countXk; k ++) {
+
+      let chosenX = 0
+
+      if (bellmanExpr.xCoeff[k] < 0) {
+        chosenX = max(xInnitial[k])
+      } else {
+        chosenX = min(xInnitial[k])
+      }
+
+      xResArray[k].push(chosenX)
+
+      xVals[k] = chosenX
+  }
   
-  for (let i = 0; i < n; i ++) {
-    const row = [];
-    for (let j = 0; j < n + 1; ++j) {
-      row.push(0);
+  const xValNext: Array<number> = new Array(countXk).fill(0)
+
+  // choose x_k(i) depends on x_k(i - 1)
+  for (let i = 1; i < n + 2; i ++) {
+
+    for (let k = 0; k < countXk; k ++) {
+
+      /*
+      * calc x_k(i) coordinate depends on
+      * x_1(i - 1), ... , x_countXk(i - 1) & u
+      */
+
+      let xkNextVal = 0
+
+      // // logging
+      // console.log('xVals(' + (i - 1) + ') = ' + xVals);
+      
+
+      xResArray[k].push( xkNextVal = calcNextXki (
+                                      { x: system[k].x, u: system[k].u},
+                                      xVals,
+                                      uResArray[i - 1]
+        )
+      )
+
+      xValNext[k] = xkNextVal
     }
-  }
-  */
 
-  for (let i = 0; i < xInnitial.length; i ++) {
+    // logging
+    for (let j = 1; j <= countXk; j ++) {
 
-    xResArray[i][0] = bellmanExpr.xCoeff[i]
+      console.log('x_' + j + '(' + i + ') = ' + xValNext[j - 1]);
+      
+    }
+
+    xVals = [...xValNext]
   }
+
+  
+
+  // logging
+  console.log("*** Results of x_k(i) ***");
+  console.log(xResArray);
+  console.log("*** Result of u(i) ***");
+  console.log(uResArray);
+  
   
   
 
@@ -91,5 +139,36 @@ export const calc = (
   return {
     x: xResArray,
     u: uResArray
+
+    // TODO bellmanSteps
   };
 };
+
+function calcNextXki(
+                    equation: {           // equation from with to calc x_k 
+                      x: number[],        // coeffs before x_k
+                      u: number           // coeff before u
+                    },
+                    xVals: Array<number>,  // values of x_k(i - 1)
+                    uVal: number          // value of u(i - 1)
+): number {
+
+  // // logging
+  // console.log('equation.x = ' + equation.x);
+  // console.log('equation.u = ' + equation.u);
+  // console.log('xVals = ' + xVals);
+  // console.log('uVal = ' + uVal);
+
+  let res = 0
+
+  const countXk = equation.x.length
+
+  for (let k = 0; k < countXk; k ++) {
+
+      res += equation.x[k] * xVals[k]
+  }
+
+  res += equation.u * uVal
+
+  return res
+}
